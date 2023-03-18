e400e851c77380b250f1ab1678c235b432c23f0a
Oakmoater <dubrovecva@gmail.com>
2023-03-18 13:44:14 +0100

    lib 2task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,14 +1,26 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+// startBtn.disabled = false;
+const inputArea = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+      console.log(selectedDates[0]);
+      dateChecker();
   },
 };
 
-// startBtn.disabled = false;
+flatpickr(inputArea, options);
+
+function dateChecker() {
+    console.log('5');
+};
