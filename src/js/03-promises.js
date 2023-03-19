9a6c4838409cfc99c469f0c79040a724ab2ee93b
Oakmoater <dubrovecva@gmail.com>
2023-03-19 22:49:14 +0100

    3done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,32 @@
+import Notiflix from 'notiflix';
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
 }
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const { elements: { delay, step, amount } } = event.currentTarget;
+  let totalDelay = Number(delay.value);
+  for (let i = 1; i <= amount.value; i++) {
+    let delaySum = totalDelay;
+    setTimeout(() => {
+      createPromise(i, delaySum)
+        .then(({ position, delay}) => {
+          Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+        });
+    }, totalDelay);
+    totalDelay += Number(step.value);
+  }
+});
